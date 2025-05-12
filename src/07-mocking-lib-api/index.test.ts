f92ea9a0ccb70d9cc020479f8bf658913f0d92ee
DanTel <DaniyarT2005@gmail.com>
2025-05-13 02:25:12 +0500

    feat: add tests for mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,62 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+const createMock = jest.fn();
+const getMock = jest.fn();
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios', () => ({
+  create: createMock,
+}));
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+    jest.unmock('axios');
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const expectedData = { message: 'Success' };
+    const expectedRelativePath = '/test';
+
+    createMock.mockReturnValue({
+      get: getMock.mockResolvedValue({ data: expectedData }),
+    });
+
+    await throttledGetDataFromApi(expectedRelativePath);
+    jest.advanceTimersByTime(5000);
+
+    expect(createMock).toHaveBeenCalledTimes(1);
+    expect(createMock).toHaveBeenLastCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const expectedData = { message: 'Success' };
+    const expectedRelativePath = '/test';
+
+    createMock.mockReturnValue({
+      get: getMock.mockResolvedValue({ data: expectedData }),
+    });
+
+    await throttledGetDataFromApi(expectedRelativePath);
+    jest.advanceTimersByTime(5000);
+
+    expect(getMock).toHaveBeenCalledWith(expectedRelativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const expectedData = { message: 'Success' };
+    const expectedRelativePath = '/test';
+
+    createMock.mockReturnValue({
+      get: getMock.mockResolvedValue({ data: expectedData }),
+    });
+
+    const result = await throttledGetDataFromApi(expectedRelativePath);
+
+    expect(result).toBe(expectedData);
   });
 });
